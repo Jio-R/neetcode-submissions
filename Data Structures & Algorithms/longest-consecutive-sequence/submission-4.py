c8dc82482280cd789809fb2aa669c78d4ed30654
Jio-R <122951878+Jio-R@users.noreply.github.com>
2026-05-24 08:02:25 -0400

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if nums == []:
+            return 0
+
+        L = [False] * (max(nums)+1)
+        
+        for n in nums:
+            print(n)
+            L[n] = True
+        print(f"L: {[[i, L[i]] for i, val in enumerate(L)]}")
+
+        last = L[0]
+        if last:
+            cur = 1
+        else:
+            cur = 0
+
+        longest = max(cur, longest)
+
+        for n in L[1:]:
+            if n:
+                cur += 1
+                longest = max(cur, longest)
+            else:
+                longest = max(cur, longest)
+                cur = 0
+
+        return longest
